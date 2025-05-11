6ddeec21328bfae71a182de9565619e319787757
karthik05122 <karthikkarthik5122@gmail.com>
2025-05-11 12:37:33 -0400

    fix copy-to-clipboard

--- libs/web/ui/copy/src/lib/web-ui-copy.tsx
+++ libs/web/ui/copy/src/lib/web-ui-copy.tsx
@@ -1,8 +1,6 @@
-import { Text, Tooltip, useToast } from '@chakra-ui/react'
-import { Button } from '@saas-ui/react'
+import { Button, Text, Tooltip, useClipboard, useToast } from '@chakra-ui/react'
 import { IconCopy } from '@tabler/icons'
 import { ReactNode } from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 export interface WebUiCopyProps {
   disabled?: boolean
@@ -11,20 +9,26 @@ export interface WebUiCopyProps {
   text?: string
 }
 
-export function WebUiCopy({ disabled, label, size, text }: WebUiCopyProps) {
+export function WebUiCopy({ disabled, label, size = 'sm', text = '' }: WebUiCopyProps) {
+  const { onCopy } = useClipboard(text)
   const toast = useToast()
 
+  const handleCopy = () => {
+    onCopy()
+    toast({
+      status: 'info',
+      title: `Copied ${text.length} characters to clipboard`,
+      duration: 2000,
+      isClosable: true,
+    })
+  }
+
   return (
-    <CopyToClipboard
-      text={text ?? ''}
-      onCopy={() => toast({ status: 'info', title: `Copied ${text?.length} characters to clipboard` })}
-    >
-      <Tooltip label={`Copy ${text?.length} characters to clipboard `} placement="top">
-        <Button p={size} variant="outline" disabled={disabled} size={size}>
-          <IconCopy color="gray" size={16} />
-          {label ? typeof label === 'string' ? <Text ml={2}>{label}</Text> : label : null}
-        </Button>
-      </Tooltip>
-    </CopyToClipboard>
+    <Tooltip label={`Copy ${text.length} characters to clipboard`} placement="top">
+      <Button p={size} variant="outline" disabled={disabled} size={size} onClick={handleCopy}>
+        <IconCopy color="gray" size={16} />
+        {label ? (typeof label === 'string' ? <Text ml={2}>{label}</Text> : label) : null}
+      </Button>
+    </Tooltip>
   )
 }
